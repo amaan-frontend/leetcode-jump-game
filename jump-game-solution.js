0f7f135b1c7aa2b44184a88a3e2c02fb5fb08256
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-06-18 22:02:20 +0530

    solution upload

---
+++ jump-game-solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+  const n = nums.length;
+  if (n === 1) return 0;
+
+  let maxReach = nums[0];
+  let steps = 1;
+  return steps;
+};
